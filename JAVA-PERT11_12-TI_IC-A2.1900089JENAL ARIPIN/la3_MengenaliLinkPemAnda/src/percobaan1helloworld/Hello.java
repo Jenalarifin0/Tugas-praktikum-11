c21a755970168499e650cc922d038987e4951d45
jenalarifin0 <ja2880514@gmail.com>
2019-12-07 14:44:25 +0700

    File finis

---
+++ JAVA-PERT11_12-TI_IC-A2.1900089JENAL ARIPIN/la3_MengenaliLinkPemAnda/src/percobaan1helloworld/Hello.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package percobaan1helloworld;
+
+/**
+ *
+ * @author HERI FIRMANSAH 
+ */
+public class Hello {
+          
+         
+         /**      * My first java program      */     
+         public static void main(String[] args) {   
+             
+        //Menampilkan kata "Hello world" dilayar         
+         System.out.println("Hello world!");  
+ 
+  
+        }   
+ 
+}
+
+    
+
---
+++ JAVA-PERT11_12-TI_IC-A2.1900089JENAL ARIPIN/la4_Dasar2Pemograman/src/percobaan8penggunaanoperatorkondisi/KondisiOperator1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package percobaan8penggunaanoperatorkondisi;
+
+/**
+ *
+ * @author HERI FIRMANSAH
+ */
+public class KondisiOperator1 {
+     public static void main( String[] args ){    
+         String   status = "";
+         int grade = 80;       
+         //mendapatkan status pelajar   
+         status = (grade >= 60)?"Passed":"Fail"; 
+ 
+   //print status    
+         System.out.println( status );  
+     }  } 
+ 
+ 
+
